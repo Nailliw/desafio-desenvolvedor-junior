72ffe9ed67a8523bbd1cb3d25c2ef092c1035db1
Willian Ribeiro <willianribeirodev@gmail.com>
2021-06-21 21:52:24 -0300

    ok

--- api/index.js
+++ api/index.js
@@ -8,7 +8,9 @@ const port = 3000;
 app.use(cors());
 routes(app);
 
-app.listen(port, () => console.log(`Server is running on port: ${port}`));
+app.listen(process.env.PORT || 3000, () =>
+  console.log(`Server is running on port: ${port}`)
+);
 
 module.exports = app;
 // Comandos básicos das migrations
